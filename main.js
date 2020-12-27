cb617e0d867ea426ea49cf2d0195cdfc95908960
gauravranganath <gauravranganath@GAURAVs-MacBook-Pro.local>
2020-12-27 14:27:55 -0500

    made some changes

--- main.js
+++ main.js
@@ -1,7 +1,43 @@
-function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
+function readMoreDepactor() {
+    var dots = document.getElementById("dots-depactor");
+    var moreText = document.getElementById("more-depactor");
+    var btnText = document.getElementById("myBtn-depactor");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read More"; 
+        moreText.style.display = "none";
+    } 
+  
+    else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read Less"; 
+        moreText.style.display = "inline";
+    }
+}
+
+function readMoreFreelance() {
+    var dots = document.getElementById("dots-freelance");
+    var moreText = document.getElementById("more-freelance");
+    var btnText = document.getElementById("myBtn-freelance");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read More"; 
+        moreText.style.display = "none";
+    } 
+  
+    else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read Less"; 
+        moreText.style.display = "inline";
+    }
+}
+
+function readMoreIEEE() {
+    var dots = document.getElementById("dots-IEEE");
+    var moreText = document.getElementById("more-IEEE");
+    var btnText = document.getElementById("myBtn-IEEE");
 
     if (dots.style.display === "none") {
         dots.style.display = "inline";
